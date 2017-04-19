db1e6b477decc60ba9304abbaed0683a276a93f0
sounan <nandi.sourav25@gmail.com>
2017-04-19 17:15:55 +0530

    fix redundant statement for import selenium.By

--- OrangeHRM/src/ValidLoginTestCase.java
+++ OrangeHRM/src/ValidLoginTestCase.java
@@ -1,7 +1,6 @@
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ValidLoginTestCase {

1dd712d7529c858bcd5696a9890cc0480c66261a
sounan <nandi.sourav25@gmail.com>
2017-04-20 10:38:12 +0530

    Fix issues with gittest project

---
+++ gittest/src/test/java/gittest/AppTest.java
@@ -0,0 +1,40 @@
+package gittest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class AppTest {
+
+//	@Test
+//	public void test() {
+//		System.out.println("This is a maven test");		
+//	}
+	
+	private WebDriver driver;
+	
+	@BeforeClass
+	public void initialize() {
+		System.setProperty("webdriver.chrome.driver", "D://Git_Project//OrangeHRM//BrowserDrivers//chromedriver.exe");
+		
+	}
+	
+	@Test
+	public void test() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://opensource.demo.orangehrmlive.com");
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin");
+		driver.findElement(By.id("btnLogin")).click();
+	}
+	
+	@AfterClass
+	public void close() {
+		driver.quit();
+	}
+	
+	
+}
